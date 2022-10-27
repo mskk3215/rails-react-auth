83f31ccb50a0d82f685e52dd5c1eda9ae3d4d80a
mskk3215 <m.kaaiweb@gmail.com>
2022-10-27 23:22:28 +0900

    frontend error 2

--- .history/frontend/src/components/pages/SignUp_20221026224858.tsx
+++ .history/frontend/src/components/pages/SignUp_20221026224858.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 import { makeStyles, Theme } from "@material-ui/core/styles";
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 // サインアップ用ページ
 const SignUp: React.FC = () => {
   const classes = useStyles();
-  const histroy = useHistory();
+  const histroy = useNavigate();
 
   const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
 
--- .history/frontend/src/components/pages/SignUp_20221027231927.tsx
+++ .history/frontend/src/components/pages/SignUp_20221027231927.tsx
@@ -1,20 +1,18 @@
 import React, { useState, useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import Button from "@material-ui/core/Button";
-import Box from "@material-ui/core/Box";
 
 import { AuthContext } from "App";
-import AlertMessage from "../utils/AlertMessages";
-import { signIn } from "lib/api/auth";
-import { SignInParams } from "interfaces/index";
+import AlertMessage from "components/utils/AlertMessage";
+import { signUp } from "lib/api/auth";
+import { SignUpParams } from "interfaces/index";
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
@@ -32,39 +30,38 @@ const useStyles = makeStyles((theme: Theme) => ({
     padding: theme.spacing(2),
     maxWidth: 400,
   },
-  box: {
-    marginTop: "2rem",
-  },
-  link: {
-    textDecoration: "none",
-  },
 }));
 
-// サインイン用ページ
-const SignIn: React.FC = () => {
+// サインアップ用ページ
+const SignUp: React.FC = () => {
   const classes = useStyles();
   const navigate = useNavigate();
 
   const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
 
+  const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
   const [alertMessageOpen, setAlertMessageOpen] = useState<boolean>(false);
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    const params: SignInParams = {
+    const params: SignUpParams = {
+      name: name,
       email: email,
       password: password,
+      passwordConfirmation: passwordConfirmation,
     };
 
     try {
-      const res = await signIn(params);
+      const res = await signUp(params);
       console.log(res);
 
       if (res.status === 200) {
-        // ログインに成功した場合はCookieに各値を格納
+        // アカウント作成と同時にログインさせてしまう
+        // 本来であればメール確認などを挟むべきだが、今回はサンプルなので
         Cookies.set("_access_token", res.headers["access-token"]);
         Cookies.set("_client", res.headers["client"]);
         Cookies.set("_uid", res.headers["uid"]);
@@ -72,7 +69,7 @@ const SignIn: React.FC = () => {
         setIsSignedIn(true);
         setCurrentUser(res.data.data);
 
-        navigate.push("/");
+        navigate("/");
 
         console.log("Signed in successfully!");
       } else {
@@ -88,8 +85,17 @@ const SignIn: React.FC = () => {
     <>
       <form noValidate autoComplete="off">
         <Card className={classes.card}>
-          <CardHeader className={classes.header} title="Sign In" />
+          <CardHeader className={classes.header} title="Sign Up" />
           <CardContent>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="Name"
+              value={name}
+              margin="dense"
+              onChange={(event) => setName(event.target.value)}
+            />
             <TextField
               variant="outlined"
               required
@@ -105,11 +111,21 @@ const SignIn: React.FC = () => {
               fullWidth
               label="Password"
               type="password"
-              placeholder="At least 6 characters"
               value={password}
               margin="dense"
               autoComplete="current-password"
               onChange={(event) => setPassword(event.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="Password Confirmation"
+              type="password"
+              value={passwordConfirmation}
+              margin="dense"
+              autoComplete="current-password"
+              onChange={(event) => setPasswordConfirmation(event.target.value)}
             />
             <Button
               type="submit"
@@ -117,20 +133,16 @@ const SignIn: React.FC = () => {
               size="large"
               fullWidth
               color="default"
-              disabled={!email || !password ? true : false} // 空欄があった場合はボタンを押せないように
+              disabled={
+                !name || !email || !password || !passwordConfirmation
+                  ? true
+                  : false
+              }
               className={classes.submitBtn}
               onClick={handleSubmit}
             >
               Submit
             </Button>
-            <Box textAlign="center" className={classes.box}>
-              <Typography variant="body2">
-                Don't have an account? &nbsp;
-                <Link to="/signup" className={classes.link}>
-                  Sign Up now!
-                </Link>
-              </Typography>
-            </Box>
           </CardContent>
         </Card>
       </form>
@@ -138,10 +150,10 @@ const SignIn: React.FC = () => {
         open={alertMessageOpen}
         setOpen={setAlertMessageOpen}
         severity="error"
-        message="Invalid emai or password"
+        message="Invalid email or password"
       />
     </>
   );
 };
 
-export default SignIn;
+export default SignUp;
